ab811d38129073aec5541d1fd05789f01a0029b1
Pedro Frohmut <pedrofrohmut@gmail.com>
2023-01-28 15:16:56 -0300

    Insertion Sort 30k - Rust

--- insertion-sort/insertion-sort.rs
+++ insertion-sort/insertion-sort.rs
@@ -1,41 +1,74 @@
-fn print_arrays(arr: [i32; 21], res: [i32; 21])
+use rand::Rng;
+use std::time::Instant;
+
+const SIZE: usize = 30_000;
+
+fn generate_array() -> [usize; SIZE]
 {
-    for (i, x) in arr.iter().enumerate() {
-        println!("Arr[{0}] = {1}", i, x);
+    let mut rng = rand::thread_rng(); // Caching rng for loop
+    let mut arr: [usize; SIZE] = core::array::from_fn(|_| 0);
+    for i in 1..SIZE + 1 {
+        let index = rng.gen_range(0..i);
+        let mut j = SIZE - 1;
+        while j > index {
+            arr[j] = arr[j - 1];
+            j -= 1;
+        }
+        arr[index] = i;
     }
-    println!("\n");
-    for (i, x) in res.iter().enumerate() {
-        println!("Res[{0}] = {1}", i, x);
+    return arr;
+}
+
+fn insertion_sort(arr: &mut [usize; SIZE])
+{
+    for i in 1..SIZE {
+        let key = arr[i];
+        let mut j = i - 1;
+        while j > 0 && arr[j] > key {
+            arr[j + 1] = arr[j];
+            j -= 1;
+        }
+        // You need assistant part because rust wont let 'j: usize' goes negative
+        // in C you just get an extra loop and j == -1
+        let mut assistant: i32 = j as i32;
+        if j == 0 && arr[j] > key {
+            arr[j + 1] = arr[j];
+            assistant -= 1;
+        }
+        arr[(assistant + 1) as usize] = key;
     }
+}
+
+fn compare_arrays(arr: &[usize], expected: &[usize]) -> bool
+{
+    for i in 0..arr.len() {
+        if arr[i] != expected[i] { return false; }
+    }
+    return true;
 }
 
 fn main()
 {
-    let arr: [i32; 21] = [5, 19, 20, 2, 15, 17, 4, 6, 14, 7, 21, 1, 16, 9, 12, 13, 3, 18, 10, 8, 11];
-    let mut res: [i32; 21] = Default::default();
+    // Generate array
+    let gen_start = Instant::now();
+    let mut arr = generate_array();
+    let gen_elapsed = gen_start.elapsed().as_millis();
 
-    if arr[0] < arr[1] {
-        res[0] = arr[0];
-        res[1] = arr[1];
+    // Sort array
+    let sort_start = Instant::now();
+    insertion_sort(&mut arr);
+    let sort_elapsed = sort_start.elapsed().as_millis();
+
+    // Eval sorted array with an expected result
+    let expected: [usize; SIZE] = core::array::from_fn(|i| i + 1);
+    let are_equal = compare_arrays(&arr, &expected);
+
+    // Output results
+    println!("1. Time to generate array: {} ms", gen_elapsed);
+    if ! are_equal {
+        println!("FAIL!\n2. The result array is NOT sorted as expected");
     } else {
-        res[0] = arr[1];
-        res[1] = arr[0];
+        println!("SUCCESS!\n2. The result array is sorted as expected");
+        println!("3. Time to sort the array: {} ms", sort_elapsed);
     }
-
-    for i in 2..arr.len() {
-        let mut j = 0;
-        while j < res.len() && res[j] < arr[i] { j += 1; }
-        if j == res.len() {
-            res[i] = arr[i];
-        } else {
-            let mut k = res.len() - 1;
-            while k > j {
-                res[k] = res[k - 1];
-                k -= 1;
-            }
-            res[j] = arr[i];
-        }
-    }
-
-    // print_arrays(arr, res);
 }
