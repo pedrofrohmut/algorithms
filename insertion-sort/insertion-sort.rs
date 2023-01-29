cc58326a64058781bf573a9650b4e48b18b4d66b
Pedro Frohmut <pedrofrohmut@gmail.com>
2023-01-29 16:30:08 -0300

    Otimization: Insertion Sort 30k - Rust

--- insertion-sort/insertion-sort.rs
+++ insertion-sort/insertion-sort.rs
@@ -1,7 +1,7 @@
 use rand::Rng;
 use std::time::Instant;
 
-const SIZE: usize = 30_000;
+const SIZE: usize = 30000;
 
 fn generate_array() -> [usize; SIZE]
 {
@@ -19,24 +19,17 @@ fn generate_array() -> [usize; SIZE]
     return arr;
 }
 
-fn insertion_sort(arr: &mut [usize; SIZE])
+fn insertion_sort(arr: &[usize; SIZE]) -> [usize; SIZE]
 {
+    let mut res: [usize; SIZE] = core::array::from_fn(|_| usize::max_value());
+    res[0] = arr[0];
     for i in 1..SIZE {
-        let key = arr[i];
-        let mut j = i - 1;
-        while j > 0 && arr[j] > key {
-            arr[j + 1] = arr[j];
-            j -= 1;
-        }
-        // You need assistant part because rust wont let 'j: usize' goes negative
-        // in C you just get an extra loop and j == -1
-        let mut assistant: i32 = j as i32;
-        if j == 0 && arr[j] > key {
-            arr[j + 1] = arr[j];
-            assistant -= 1;
-        }
-        arr[(assistant + 1) as usize] = key;
+        let mut j = 0;
+        while j < SIZE - 1 && res[j] < arr[i] { j += 1; }
+        res[j..].rotate_right(1);
+        res[j] = arr[i];
     }
+    return res;
 }
 
 fn compare_arrays(arr: &[usize], expected: &[usize]) -> bool
@@ -51,17 +44,17 @@ fn main()
 {
     // Generate array
     let gen_start = Instant::now();
-    let mut arr = generate_array();
+    let arr = generate_array();
     let gen_elapsed = gen_start.elapsed().as_millis();
 
     // Sort array
     let sort_start = Instant::now();
-    insertion_sort(&mut arr);
+    let res = insertion_sort(&arr);
     let sort_elapsed = sort_start.elapsed().as_millis();
 
     // Eval sorted array with an expected result
     let expected: [usize; SIZE] = core::array::from_fn(|i| i + 1);
-    let are_equal = compare_arrays(&arr, &expected);
+    let are_equal = compare_arrays(&res, &expected);
 
     // Output results
     println!("1. Time to generate array: {} ms", gen_elapsed);
