8ab799d25b408635730e92cb314585285832b7ac
Pedro Frohmut <pedrofrohmut@gmail.com>
2023-01-31 15:43:49 -0300

    Merge Sort 30k - Python

--- merge-sort/merge-sort.py
+++ merge-sort/merge-sort.py
@@ -1,30 +1,44 @@
 #! /usr/bin/env python
 
-import math
+from random import random
+from math import floor
+from time import time
+from array import array
 
-def order_array(arr):
-    res =  []
-    res.append(arr[0])
-    for i in range(1, len(arr)):
-        j = 0
-        while j < len(res) and res[j] < arr[i]:
-            j += 1
-        if j >= i:
-            res.append(arr[i])
-        else:
-            res.insert(j, arr[i])
-    return res
+def get_index(i):
+    return floor(random() * i)
+
+
+def generate_array(size):
+    arr = array("I", [])
+    for i in range(1, size + 1):
+        index = get_index(i)
+        arr.insert(index, i)
+    return arr
+
 
 def get_left_and_right(arr):
-    mid = math.floor(len(arr) / 2)
-    Left = arr[0:mid]
-    Right = arr[mid:]
-    return Left, Right
+    extra = 0 if len(arr) % 2 == 0 else 1
+    mid = floor(len(arr) / 2)
+    left = arr[0:mid + extra]
+    right = arr[mid + extra:]
+    return left, right
+
+
+def order_array(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+        while (j >= 0 and arr[j] > key):
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+    return arr
 
 
 def merge_left_and_right(Left, Right):
     res_len = len(Left) + len(Right)
-    res = []
+    res = array("I", [])
     left_index = 0
     right_index = 0
     for i in range(0, res_len):
@@ -43,14 +57,47 @@ def merge_left_and_right(Left, Right):
     return res
 
 
-# arr is the full array with 40 elements
-arr = [5, 30, 34, 19, 20, 31, 33, 2, 15, 32, 25, 17, 38, 4, 6, 23, 29, 40, 14, 7, 36, 21, 1, 16, 39, 26, 35, 9, 12, 13, 24, 3, 37, 18, 27, 10, 8, 28, 11, 22]
+def compare_arrays(subject, expected):
+    for i in range(0, len(subject)):
+        if subject[i] != expected[i]:
+            return False
+    return True
 
-Left, Right = get_left_and_right(arr)
 
-Left = order_array(Left)
-Right = order_array(Right)
+def main():
+    size = 30000
 
-res = merge_left_and_right(Left, Right)
+    # Generate array
+    gen_start = time()
+    arr = generate_array(size)
+    gen_end = time()
 
-# for i, v in enumerate(res): print(f"res[{i}] = {v}")
+    sort_start = time()
+
+    # Get Left and Right
+    left, right = get_left_and_right(arr)
+
+    # Sort Left and Right (insertion sort)
+    left = order_array(left)
+    right = order_array(right)
+
+    # Merge Left and Right
+    merged = merge_left_and_right(left, right)
+
+    sort_end = time()
+
+    # Eval sorted array with an expected result
+    expected = array("I", [])
+    for i in range(1, size + 1):
+        expected.append(i)
+    are_equal = compare_arrays(merged, expected)
+
+    # Output the results
+    print(f"1. Time to generate array: {floor((gen_end - gen_start) * 1000)} ms")
+    if not are_equal:
+        print("2. Result array is NOT sorted as expected")
+    else:
+        print("2. Result array is sorted as expected")
+        print(f"3. Time to merge sort the array: {floor((sort_end - sort_start) * 1000)} ms")
+
+main()
