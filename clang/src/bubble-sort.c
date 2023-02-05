73556a8fd141b6c01cf960bd80a9c1f1df7e4788
Pedro Frohmut <pedrofrohmut@gmail.com>
2023-02-05 17:02:44 -0300

    Bubble Sort 30k - C memcpy

--- clang/src/bubble-sort.c
+++ clang/src/bubble-sort.c
@@ -1,20 +1,26 @@
 #include <stdio.h>
+#include <string.h>
 #include <time.h>
 
 #include "../headers/Arrays.h"
 #include "../headers/Time.h"
 
-void bubble_sort(int * arr, int size)
+int * bubble_sort(int * arr, int size)
 {
+    int * res = malloc(sizeof(int) * size);
+    memcpy(res, arr, sizeof(int) * size);
+
     for (int i = 0; i < size; i++) {
         for (int j = size - 1; j > i; j--) {
-            if (arr[j] < arr[j - 1]) {
-                const int temp = arr[j];
-                arr[j] = arr[j - 1];
-                arr[j - 1] = temp;
+            if (res[j] < res[j - 1]) {
+                const int temp = res[j];
+                res[j] = res[j - 1];
+                res[j - 1] = temp;
             }
         }
     }
+
+    return res;
 }
 
 int main()
@@ -28,15 +34,19 @@ int main()
 
     // Sort array
     const clock_t sort_start = clock();
-    bubble_sort(arr, size);
+    int * res = bubble_sort(arr, size);
     const clock_t sort_end = clock();
 
     // Eval sorted array with an expected result
     int * expected = generate_expected_array(size);
 
+    /* print_array(arr, size, "Arr"); */
+    /* print_array(res, size, "Res"); */
+    /* print_array(expected, size, "Expected"); */
+
     // Output the results
     printf("Time to generate the array: %.4f ms\n", get_time_ms(gen_start, gen_end));
-    if (! is_sorted_as_expected(arr, expected, size)) {
+    if (! is_sorted_as_expected(res, expected, size)) {
         printf("2. The result array is NOT sorted as expected\n");
     } else {
         printf("2. The result array is sorted as expected\n");
@@ -44,6 +54,7 @@ int main()
     }
 
     free(arr);
+    free(res);
     free(expected);
     return 0;
 }
