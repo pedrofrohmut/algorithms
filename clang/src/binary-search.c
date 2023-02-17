e9a8ace390a760d5a35210d61ccccde1c4b0696c
Pedro Frohmut <pedrofrohmut@gmail.com>
2023-02-16 22:36:29 -0300

    Recursion on Binary Search and improvements

--- clang/src/binary-search.c
+++ clang/src/binary-search.c
@@ -3,6 +3,9 @@
 #include "../headers/Arrays.h"
 #include "../headers/Time.h"
 
+typedef struct { int index; int value; } Position_Pairs;
+typedef struct { Position_Pairs * arr; double time; } Search_Result;
+
 const int binary_search(const int arr[], int start, int end, const int value)
 {
     while (end != start) {
@@ -11,41 +14,46 @@ const int binary_search(const int arr[], int start, int end, const int value)
         if (arr[mid] > value) { end = mid - 1; }
         if (arr[mid] < value) { start = mid + 1; }
     }
-    if (arr[end] != value) {
-        return -1;
-    }
+    if (arr[end] != value) return -1;
     return end;
 }
 
-Result bench_binary_search(const int ord_arr[], const int rnd_arr[], const int size)
+const int recursion_binary_search(const int arr[], const int start, const int end, const int value)
 {
-    int * positions = malloc(sizeof(int) * size);
+    if (start != end) {
+        const int mid = ((end - start + 1) / 2) + start;
+        if (arr[mid] == value) return mid;
+        if (arr[mid] > value) {
+            return recursion_binary_search(arr, start, mid - 1, value);
+        }
+        if (arr[mid] < value) {
+            return recursion_binary_search(arr, mid + 1, end, value);
+        }
+    }
+    if (arr[end] != value) return -1;
+    return end;
+}
+
+Search_Result bench_binary_search(const int ord_arr[], const int rnd_arr[], const int size)
+{
+    Position_Pairs * positions = malloc(sizeof(Position_Pairs) * size);
     const clock_t start = clock();
     for (int i = 0; i < size; i++) {
         const int value = rnd_arr[i];
         const int start = 0;
         const int end = size - 1;
-        const int pos = binary_search(ord_arr, start, end, value);
-        positions[i] = pos;
+        const int pos = recursion_binary_search(ord_arr, start, end, value);
+        positions[i] = (Position_Pairs) { pos, value };
     }
     const clock_t end = clock();
     const double search_time = get_time_ms(start, end);
-    return (Result) {positions, search_time};
+    return (Search_Result) {positions, search_time};
 }
 
-int * generate_expected_positions_array(const int rnd_arr[], const int size)
-{
-    int * expected = malloc(sizeof(int) * size);
-    for (int i = 0; i < size; i++) {
-        expected[i] = rnd_arr[i] - 1;
-    }
-    return expected;
-}
-
-bool is_positions_as_expected(int positions[], int expected[], const int size)
+bool is_positions_as_expected(Position_Pairs pairs[], int ordered[], const int size)
 {
     for (int i = 0; i < size; i++) {
-        if (positions[i] != expected[i]) return false;
+        if (pairs[i].value != ordered[pairs[i].index]) return false;
     }
     return true;
 }
@@ -55,17 +63,14 @@ int main()
     const int size = 40000;
 
     // Generate Shuffled Array
-    int * rnd_arr = generate_shuffled_array(size);
+    int * rnd_arr = generate_random_values_array(size);
     int * ord_cpy = get_ordered_copy(rnd_arr, size);
 
     // Bench binary search all results
-    Result sch_res = bench_binary_search(ord_cpy, rnd_arr, size);
-
-    // Eval search results with expected positions
-    int * expected = generate_expected_positions_array(rnd_arr, size);
+    Search_Result sch_res = bench_binary_search(ord_cpy, rnd_arr, size);
 
     // Output the results
-    if (! is_positions_as_expected(sch_res.arr, expected, size)) {
+    if (! is_positions_as_expected(sch_res.arr, ord_cpy, size)) {
         printf("1. The positions are NOT as expected\n");
     } else {
         printf("1. The positions are as expected\n");
@@ -76,6 +81,6 @@ int main()
     free(rnd_arr);
     free(ord_cpy);
     free(sch_res.arr);
-    free(expected);
+
     return 0;
 }
