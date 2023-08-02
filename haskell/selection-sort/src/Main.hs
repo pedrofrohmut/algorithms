9a9fc5cb60d6a8dbeb124a4d18ee0dd84e87dada
Pedro Frohmut <pedrofrohmut@gmail.com>
2023-08-02 16:58:37 -0300

    Haskell - selection sort

--- haskell/selection-sort/src/Main.hs
+++ haskell/selection-sort/src/Main.hs
@@ -20,11 +20,45 @@ genArr size  len gen  =
         value = floor $ (rnd_value * size) + 1
     in  value : genArr size (len - 1) newGen
 
+getSmaller :: [Int] -> Int
+getSmaller mainArr =
+        getIt (head mainArr) mainArr
+    where
+        getIt minv [] = minv
+        getIt minv arr =
+            if (head arr) < minv
+                then getIt (head arr) (tail arr)
+                else getIt minv (tail arr)
+
+removeFromArr :: Int -> [Int] -> [Int]
+removeFromArr _val [] = []
+removeFromArr val (x:xs) =
+    if x == val
+        then removeFromArr val xs
+        else x : removeFromArr val xs
+
+selectSort :: [Int] -> [Int]
+selectSort mainArr =
+    go [] mainArr
+    where
+        go result [] = result
+        go result arr =
+            let minv = getSmaller arr in
+            let newArr = removeFromArr minv arr in
+            go (result ++ [minv]) newArr
+
 main :: IO ()
 main = do
     let size = 1000000 :: Float
-    let len = 1000 :: Int
-    let initialGen = (mkStdGen 1)
+    let len = 10 :: Int
+    let initialGen = (mkStdGen 2)
     let arr = genArr size len initialGen
-    -- putStrLn $ show arr
-    putStrLn "Arr generated"
+
+    putStrLn $ show arr ++ "\n"
+
+    let smaller = getSmaller arr
+    putStrLn $ "Min: " ++ (show smaller) ++ "\n"
+
+    putStrLn $ "Without Min: " ++ (show $ removeFromArr smaller arr) ++ "\n"
+
+    putStrLn $ "Result: " ++ (show $ selectSort arr)
