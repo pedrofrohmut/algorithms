75f34fb2c56e819be09214e40858763365ed0d5c
Pedro Frohmut <pedrofrohmut@gmail.com>
2023-08-22 10:14:05 -0300

    Haskell fizzbuzz with map

--- haskell/fizzbuzz.hs
+++ haskell/fizzbuzz.hs
@@ -1,14 +1,27 @@
 fizzbuzz :: Int -> [String]
 fizzbuzz n =
     let
-        helper :: Int -> Int -> [String]
-        helper num i
+        iterator :: Int -> [String]
+        iterator i
             | i > n = []
             | mod i 15 == 0 = "FizzBuzz" : goNext
             | mod i 5 == 0 = "Buzz" : goNext
             | mod i 3 == 0 = "Fizz" : goNext
             | otherwise = (show i) : goNext
             where
-                goNext = helper n (i + 1)
+                goNext = iterator (i + 1)
     in
-        helper n 1
+        iterator 1
+
+fizzbuzzMap :: Int -> [String]
+fizzbuzzMap n =
+    let
+        fizzbuzzIt :: Int -> String
+        fizzbuzzIt x
+            | mod x 15 == 0 = "FizzBuzz"
+            | mod x 5 == 0 = "Buzz"
+            | mod x 3 == 0 = "Fizz"
+            | otherwise = show x
+
+    in
+        map fizzbuzzIt [1..n]
