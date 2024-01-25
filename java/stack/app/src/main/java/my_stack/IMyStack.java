f05f69b4bf9ab1c22efcd0c7826f96de943a5137
Pedro Frohmut <pedrofrohmut@gmail.com>
2024-01-25 14:55:07 -0300

    Comments on stack

--- java/stack/app/src/main/java/my_stack/IMyStack.java
+++ java/stack/app/src/main/java/my_stack/IMyStack.java
@@ -1,13 +1,18 @@
 package my_stack;
 
+/*
+    Stack: LIFO data structure. Last In First Out. stores objects into a sort of "vertical tower"
+*/
 public interface IMyStack<T> {
     // Tests if this stack is empty.
     boolean isEmpty();
 
-    // Looks at the object at the top of this stack without removing it from the stack.
+    // Looks at the object at the top of this stack without removing it from the
+    // stack.
     T peek();
 
-    // Removes the object at the top of this stack and returns that object as the value of this function.
+    // Removes the object at the top of this stack and returns that object as the
+    // value of this function.
     T pop();
 
     // Pushes an item onto the top of this stack.
