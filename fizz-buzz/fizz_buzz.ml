0427e9e60f9d17f9ef860b90671878220306bb3c
pedrofrohmut <pedrofrohmut@gmail.com>
2026-07-24 10:50:26 -0300

    fizz buzz in ocaml

---
+++ fizz-buzz/fizz_buzz.ml
@@ -0,0 +1,47 @@
+(*
+  fizz_buzz.ml
+  Pedro Frohmut 2026 Copyrights
+
+  Fizz Buzz algorithm: it returns Fizz to multiple of 3, Buzz to multiple of 5 and
+  FizzBuzz when it is multiple of 3 and 5.
+ *)
+
+(* TODO: make a fizz buzz with a custom type instead of just strings *)
+(* Regular recursion with callstack unwinding at the end *)
+let fizz_buzz (limit: int): string list =
+  let rec fizz_buzz (limit: int): string list =
+    match limit with
+    | x when x < 0 -> failwith "Fizz Buzz only support positive integer numbers"
+    | 0 -> []
+    | x when x mod 15 == 0 -> "FizzBuzz" :: fizz_buzz (limit - 1)
+    | x when x mod 5 == 0  -> "Buzz" :: fizz_buzz (limit - 1)
+    | x when x mod 3 == 0  -> "Fizz" :: fizz_buzz (limit - 1)
+    | x -> string_of_int x :: fizz_buzz (limit - 1)
+  in
+  List.rev (fizz_buzz limit)
+
+(* Tail call recursion with accumulator, counter and without callstack unwinding
+   when the base case is reached *)
+let fizz_buzz2 (limit: int): string list =
+  let rec aux (acc: string list) (counter: int) (limit: int): string list =
+    match counter with
+    | i when i < 0 -> failwith "Fizz Buzz only support positive integer numbers"
+    | i when i > limit -> acc
+    | i when i mod 15 == 0 -> aux ("FizzBuzz" :: acc) (i + 1) limit
+    | i when i mod 5 == 0  -> aux ("Buzz" :: acc) (i + 1) limit
+    | i when i mod 3 == 0  -> aux ("Fizz" :: acc) (i + 1) limit
+    | i -> aux (string_of_int i :: acc) (i + 1) limit
+  in
+  List.rev (aux [] 1 limit)
+
+let fizz_buzz_printer (fizz_buzz_fun: int -> string list) (limit: int): unit =
+  let seq = fizz_buzz_fun limit |> String.concat ", " in
+  let out = Printf.sprintf "FizzBuzz => [%s]" seq in
+  print_endline out
+
+let main (): unit =
+  fizz_buzz_printer fizz_buzz 100;
+  fizz_buzz_printer fizz_buzz2 100
+
+let () =
+  main ()
